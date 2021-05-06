30f765d2bfc2c25eaf3313565e6308ffc1b6ce8a
Fermain <diplomat@fermain.io>
2021-05-06 16:35:36 +0200

    Improves baseURL handling and adds a public api

--- src/services/open-exchange-rates.service.ts
+++ src/services/open-exchange-rates.service.ts
@@ -3,38 +3,48 @@ export interface CurrencyList {
 }
 
 export class OpenExchangeRatesService {
-    constructor(private appId: string, private baseUrl = 'https://openexchangerates.org/api') {}
+    private baseUrl: URL
+
+    constructor (private appId: string, private apiPath = 'https://openexchangerates.org/') {
+      this.baseUrl = new URL(apiPath)      
+    }
 
     /**
      * ## Prototype Fetch
      * _A lightweight wrapper around browser fetch API._
-     * 
-     * Allows for casting to user defined models, 
+     *
+     * Allows for casting to user defined models,
      * and provides surface area for notification and error handlers.
      *
      */
-    private async fetch<T>(url: URL) {
-        try {
-            const response = await fetch(url.href)
-            return await response.json() as T
-        } catch(error) {
-            // Error reporting service - There was a network error
-            return {}
-        }
+    private async fetch<T> (url: URL, options?: RequestInit) {
+      try {
+        const response = await fetch(url.href, options)
+        return await response.json() as T
+      } catch (error) {
+      // Error reporting service - There was a network error
+        return {}
+      }
     }
 
     /**
-     * ## Get all supported currencies
+     * ## GET all supported currencies
      * _Retrieve all currencies supported by the API._
-     * 
+     *
      * Returns a keyvalue indexed object of currencies that can be queried.
      *
      */
-    private async getSupportedCurrencies(): Promise<CurrencyList> {
-        const url = new URL(`${this.baseUrl}/currencies.json`)
+    private async getSupportedCurrencies (): Promise<CurrencyList> {
+      const url = new URL('api/currencies.json', this.baseUrl)
 
-        return await this.fetch<CurrencyList>(url);
+      return await this.fetch<CurrencyList>(url, {
+        method: 'GET'
+      })
     }
 
-    
+    public get request () {
+      return {
+        currencies: () => this.getSupportedCurrencies()
+      }
+    }
 }
