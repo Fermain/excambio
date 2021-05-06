9963ba8241b4ef065ff572bc8ae89db1b895dde2
Fermain <diplomat@fermain.io>
2021-05-06 15:22:40 +0200

    Adds blank component for scaffolding

---
+++ src/components/Blank.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="blank">
+    <p>This is a blank component</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component'
+
+export default class Blank extends Vue {}
+</script>
+
+<style scoped lang="scss">
+
+</style>
