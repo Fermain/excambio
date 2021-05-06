272da14cdc856180b9396b1cdcd48b500f51bfd3
Fermain <diplomat@fermain.io>
2021-05-06 21:46:15 +0200

    Migrated service instance to vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,41 @@
+import { CurrencyNames, CurrencyRates, LatestRates, OpenExchangeRatesService } from '@/services/open-exchange-rates.service'
 import { createStore } from 'vuex'
+
+const service = new OpenExchangeRatesService(process.env.VUE_APP_OPENAPI_API_ID)
 
 export default createStore({
   state: {
+    names: {},
+    rates: {}
   },
   mutations: {
+    updateCurrencyNames (state, names: CurrencyNames) {
+      state.names = names
+    },
+    updateCurrencyRates (state, rates: LatestRates) {
+      state.rates = rates
+    }
   },
   actions: {
-  },
-  modules: {
+    async getCurrencyNames (context) {
+      const names = await service.request.names()
+      context.commit('updateCurrencyNames', names)
+    },
+    updateCurrencyNames (context, names: CurrencyNames) {
+      context.commit('updateCurrencyNames', names)
+    },
+    resetCurrencyNames (context) {
+      context.commit('updateCurrencyNames', {})
+    },
+    async getCurrencyRates (context) {
+      const rates = await service.request.rates()
+      context.commit('updateCurrencyRates', rates)
+    },
+    updateCurrencyRates (context, rates: LatestRates) {
+      context.commit('updateCurrencyRates', rates)
+    },
+    resetCurrencyRates (context) {
+      context.commit('updateCurrencyRates', {})
+    }
   }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,33 @@
 <template>
   <div class="home">
     <h1>Excambio</h1>
-    <ul>
-      <li v-for="(value, key) in currencies" :key="key">
-        <b>{{ key }}:</b> {{value}}
-      </li>
-    </ul>
   </div>
 </template>
 
 <script lang="ts">
-import { CurrencyNames, OpenExchangeRatesService } from '@/services/open-exchange-rates.service'
-import { Vue } from 'vue-class-component'
+import { Options, Vue } from 'vue-class-component'
+import { mapActions } from 'vuex'
 
+@Options({
+  methods: {
+    ...mapActions({
+      getCurrencyNames: 'getCurrencyNames',
+      resetCurrencyNames: 'resetCurrencyNames',
+      getCurrencyRates: 'getCurrencyRates',
+      resetCurrencyRates: 'resetCurrencyRates'
+    })
+  }
+})
 export default class Home extends Vue {
-  private service = new OpenExchangeRatesService('')
-
-  private currencies: CurrencyNames = {}
+  getCurrencyNames!: () => any;
+  resetCurrencyNames!: () => any;
+  getCurrencyRates!: () => any;
+  resetCurrencyRates!: () => any;
 
   async mounted (): Promise<void> {
-    this.currencies = await this.service.request.currencies()
+    this.getCurrencyNames()
+    this.getCurrencyRates()
+    console.log(this)
   }
 }
 </script>
