1254ecee596d937aad6bc72e10f6c5bb204a98c4
Fermain <diplomat@fermain.io>
2021-05-07 12:27:33 +0200

    Adds terms of use template page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
+import Terms from '../views/Terms.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/terms',
+    name: 'Terms of Use',
+    component: Terms
   }
 ]
 
