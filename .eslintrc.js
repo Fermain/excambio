27cb6ed6378693dd53bb23e54408c0648b7a64d4
Fermain <diplomat@fermain.io>
2021-05-07 08:53:48 +0200

    Minro fix to eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'on' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'explicit-module-boundary-types': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   },
   overrides: [
