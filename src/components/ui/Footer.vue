52c4bac0645f146f419576b3e691cb680caa203a
Fermain <diplomat@fermain.io>
2021-05-07 12:28:30 +0200

    Adds basic chrome to UI elements including logo

--- src/components/ui/Footer.vue
+++ src/components/ui/Footer.vue
@@ -1,14 +1,50 @@
 <template>
   <footer class="ui-footer">
+    <router-link class="logo" to="/">
+      <Pangol />
+    </router-link>
+    <nav>
+      <router-link to="/terms">Terms of Use</router-link>
+    </nav>
   </footer>
 </template>
 
 <script lang="ts">
-import { Vue } from 'vue-class-component'
+import { Options, Vue } from 'vue-class-component'
+import Pangol from '@/components/brand/Pangol.vue'
 
+@Options({
+  components: {
+    Pangol
+  }
+})
 export default class UiFooter extends Vue {}
 </script>
 
 <style scoped lang="scss">
+.ui-footer {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
+  position: sticky;
+  bottom: 0;
+  left: 0;
+  right: 0;
+
+  padding: 0.5rem;
+
+  filter: invert(1);
+  background: #ece9e7;
+
+  nav {
+    a {
+      margin: 0.25rem;
+    }
+  }
+
+  a {
+    color: inherit;
+  }
+}
 </style>
