c387a49788582d55ddefa2440f61151a2b835471
Fermain <diplomat@fermain.io>
2021-05-07 10:31:03 +0200

    Adds exchange components

---
+++ src/components/exchange/List.vue
@@ -0,0 +1,14 @@
+<template>
+  <ul class="exchange-list">
+  </ul>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component'
+
+export default class ExchangeList extends Vue {}
+</script>
+
+<style scoped lang="scss">
+
+</style>
