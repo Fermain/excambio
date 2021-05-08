02a68b668269c7a92f0a3a46c8705c3d1fe25880
Fermain <diplomat@fermain.io>
2021-05-09 01:26:50 +0200

    Switch to full screen list view

--- src/components/exchange/List.vue
+++ src/components/exchange/List.vue
@@ -1,31 +1,33 @@
 <template>
   <table>
     <thead>
-      <tr>
-        <th v-for="column in columns" :key="column">{{ column }}</th>
+      <tr v-if="currencies.length || selected.length">
+        <th>Code</th>
+        <th>Name</th>
+        <th>Amount</th>
       </tr>
-      <tr v-for="currency in selected" :key="currency.code" class="selection" @click="onRemove(currency)">
-        <td>
+      <tr v-for="currency in selected" :key="currency.code" class="selection">
+        <td @click="onRemove(currency)">
           <b>{{ currency.code }}</b>
         </td>
-        <td>
+        <td @click="onRemove(currency)">
           {{ currency.name }}
         </td>
         <td>
-          <code v-if="currency.rate">{{ currency.rate }}</code>
+          <input type="number" v-model="values[currency.code]" @change="onValueChange($event, currency)">
         </td>
       </tr>
     </thead>
     <tbody>
-      <tr v-for="currency in currencies" :key="currency.code" @click="onSelect(currency)">
-        <td>
+      <tr v-for="currency in currencies" :key="currency.code">
+        <td @click="onSelect(currency)">
           <b>{{ currency.code }}</b>
         </td>
-        <td>
+        <td @click="onSelect(currency)">
           {{ currency.name }}
         </td>
         <td>
-          <code v-if="currency.rate">{{ currency.rate }}</code>
+          <input type="number" v-model="values[currency.code]" @change="onValueChange($event, currency)">
         </td>
       </tr>
     </tbody>
@@ -36,7 +38,7 @@
 import { key } from '@/store'
 import { Vue } from 'vue-class-component'
 import { useStore } from 'vuex'
-import { Currency } from '@/models'
+import { Currency, CurrencyRates } from '@/models'
 
 export default class ExchangeList extends Vue {
   private store = useStore(key);
@@ -46,7 +48,7 @@ export default class ExchangeList extends Vue {
   }
 
   public get currencies (): Currency[] {
-    return this.store.getters.currencyList.filter((currency: Currency) => {
+    return this.store.getters.currencyListSearchFilter.filter((currency: Currency) => {
       return !this.selected.find(selection => selection.code === currency.code)
     })
   }
@@ -59,6 +61,25 @@ export default class ExchangeList extends Vue {
     return []
   }
 
+  public set valueUSD (value: number) {
+    this.store.dispatch('setConversionValue', value)
+  }
+
+  public get valueUSD (): number {
+    return this.store.state.value
+  }
+
+  public get values (): CurrencyRates {
+    let values = {}
+    for (const code in this.store.state.latest.rates) {
+      values = {
+        ...values,
+        [code]: this.valueUSD * this.store.state.latest.rates[code]
+      }
+    }
+    return values
+  }
+
   public onSelect (currency: Currency) {
     this.store.dispatch('pushSelected', currency)
   }
@@ -66,12 +87,20 @@ export default class ExchangeList extends Vue {
   public onRemove (currency: Currency) {
     this.store.dispatch('removeSelected', currency)
   }
+
+  public onValueChange ({ target }: Event, currency: Currency) {
+    const value = Number((target as HTMLInputElement).value)
+    console.log(value, currency.rate, value / currency.rate)
+    this.valueUSD = value / currency.rate
+  }
 }
 </script>
 
 <style scoped lang="scss">
 table {
   border-spacing: 0;
+  background: var(--color-light);
+  flex: 1;
 
   thead {
     th {
